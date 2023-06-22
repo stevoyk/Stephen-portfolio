af3994bc1de581fd64d963edf6dece699cbbad87
stevoyk <70204787+stevoyk@users.noreply.github.com>
2023-06-22 16:30:46 +0100

    Update ResumeNew.js

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -8,7 +8,7 @@ import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
-const resumeLink ="https://raw.githubusercontent.com/soumyajit4419/portfolio/master/src/Assets/Soumyajit_Behera-BIT_MESRA.pdf";
+const resumeLink ="https://raw.githubusercontent.com/stevoyk/Stephen-portfolio/master/src/Assets/AS.pdf";
 
 
 function ResumeNew() {
